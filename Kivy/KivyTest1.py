00889034d13c929b7e4b59de2a69534e9e268fc4
amonsain <Augustin@pc29.home>
2016-01-03 14:48:32 +0100

    1st radio button play with Kivy

--- Kivy/KivyTest1.py
+++ Kivy/KivyTest1.py
@@ -6,18 +6,27 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-
+import subprocess
 
 
 class ScatterTextWidget(BoxLayout):
 	pass
 
 class TutorialApp(App):
+
 	def build(self):
+		self.playing = False
 		return ScatterTextWidget()
 
-	def play_radio(self):
+	def toggle_radio(self):
 		print('bouton presse')
+		if self.playing:
+			subprocess.check_output(["mpc","stop"])
+			self.playing = False
+		else:
+			subprocess.check_output(["mpc","play","1"])
+			self.playing = True
+
 
 if __name__ == "__main__":
 	TutorialApp().run()
